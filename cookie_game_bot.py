359ed036e801266d04de06060aaea7b2100faf4a
alanleew <alanleew412@gmail.com>
2025-03-27 15:21:33 -0700

    Final draft

--- cookie_game_bot.py
+++ cookie_game_bot.py
@@ -3,32 +3,43 @@ from selenium.webdriver.common.by import By
 import time
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True) # This is to keep the browser open
+options.add_experimental_option("detach", True)  # This is to keep the browser open
 driver = webdriver.Chrome(options=options)
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
 # Uses Selenium to find elements
 cookie_button = driver.find_element(By.CSS_SELECTOR, "#cookie")
 money = driver.find_element(By.CSS_SELECTOR, '#money')
-right_panel = driver.find_elements(By.CSS_SELECTOR, "#rightPanel b")
 
-# Creates dict to contain store item name and price, with special char removed
-store = {}
-for store_item in right_panel:
-    if store_item.text:   # Filters to omit blank text from HTML
-        text = store_item.text.split(" - ")
-        store[text[0]] = text[1].replace(",","")    # Creates dictionary entry
+def choose_store_item():
+    """Returns the item name to allow Selenium to click according to the button name"""
+    right_panel = driver.find_elements(By.CSS_SELECTOR, "#rightPanel b")
+    store = {}  # Creates dict to contain store item and price
+    for store_item in right_panel:
+        if store_item.text:  # Filters to omit blank text from HTML
+            text = store_item.text.split(" - ")
+            store[text[0]] = int(text[1].replace(",", "")) # Adds entry to dict
 
-# Creating while loop to run game bot
-game_is_on = True
+    # Iterates from highest to lowest price to return the item name
+    for key, value in reversed(store.items()):
+        if int(money.text.replace(",", "")) > value:
+            chosen_item = driver.find_element(By.CSS_SELECTOR, f"#buy{key}")
+            chosen_item.click()
+            return
+
+game_is_on = True  # Creating while loop to run game bot
+game_timer_start = time.time()
+game_duration = 60 # [seconds]
 while game_is_on:
-    timer_start = time.time()
-    duration = 5  # [seconds]
-    while time.time() < (timer_start + duration):   # This while loop does action for duration set above
-        cookie_button.click()
-    print(f"{duration} seconds have passed. There are {money.text} cookies.")
-
-print(store)
-
+    click_timer_start = time.time()
+    if (click_timer_start - game_timer_start) < game_duration:
+        click_duration = 10 # [seconds]
+        while time.time() < (click_timer_start + click_duration):  # This while loop does action for duration set above
+            cookie_button.click()
+        # print(f"{duration} seconds have passed. There are {money.text} cookies.")
+        choose_store_item()
+    else:
+        print(f"{game_duration} seconds have passed.\nYou ended with {money.text} cookies!")
+        break
 
 driver.quit()
